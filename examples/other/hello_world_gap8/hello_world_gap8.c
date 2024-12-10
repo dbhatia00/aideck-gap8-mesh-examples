35158b7665acd94bbb6bf7661944ae9b65d04628
Dev Bhatia <dev_bhatia@brown.edu>
2024-12-10 18:41:05 -0500

    Wifi Initializer

--- examples/other/hello_world_gap8/hello_world_gap8.c
+++ examples/other/hello_world_gap8/hello_world_gap8.c
@@ -1,44 +1,61 @@
-/**
- * ,---------,       ____  _ __
- * |  ,-^-,  |      / __ )(_) /_______________ _____  ___
- * | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
- * | / ,--´  |    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
- *    +------`   /_____/_/\__/\___/_/   \__,_/ /___/\___/
- *
- * AI-deck GAP8
- *
- * Copyright (C) 2022 Bitcraze AB
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, in version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * Hello world example
- */
+#include <stdint.h>
+#include <stdbool.h>
 #include "pmsis.h"
-#include "bsp/bsp.h"
 #include "cpx.h"
+#include "bsp/bsp.h"
+#include "wifi.h"
 
-void start_example(void)
-{
-  pi_bsp_init();
-  cpxInit();
-   while (1)
-  {
-      cpxPrintToConsole(LOG_TO_CRTP, "Hello World\n");
-      pi_time_wait_us(1000*1000);
-  }
+void setupWiFi(void) {
+    CPXPacket_t txp;
+    txp.route.destination = CPX_T_ESP32;
+    txp.route.function = CPX_F_WIFI_CTRL;
+    txp.route.version = CPX_VERSION;
+
+    WiFiCTRLPacket_t *wifiCtrl = (WiFiCTRLPacket_t *) txp.data;
+
+    // Set a placeholder SSID
+    wifiCtrl->cmd = WIFI_CTRL_SET_SSID;
+    static char ssid[] = "MESH_PLACEHOLDER";
+    memcpy(wifiCtrl->data, ssid, sizeof(ssid));
+    txp.dataLength = sizeof(ssid);
+    cpxSendPacketBlocking(&txp);
+
+    cpxPrintToConsole(LOG_TO_CRTP, "Placeholder SSID set.\n");
+
+    // Send the connect command
+    wifiCtrl->cmd = WIFI_CTRL_WIFI_CONNECT;
+    wifiCtrl->data[0] = 0x00; // Station mode
+    wifiCtrl->data[1] = 0x00; // Explicitly set station mode
+    txp.dataLength = 2;       // Send 2 bytes of data
+    cpxSendPacketBlocking(&txp);
+
+    cpxPrintToConsole(LOG_TO_CRTP, "WiFi connect command sent.\n");
 }
 
-int main(void)
-{
-  return pmsis_kickoff((void *)start_example);
+
+void start_example(void) {
+    // Initialize the GAP8 system
+    pi_bsp_init();
+    cpxInit();
+    cpxEnableFunction(CPX_F_WIFI_CTRL);
+
+    cpxPrintToConsole(LOG_TO_CRTP, "-- Starting Wi-Fi Setup for ESP-MESH --\n");
+
+    // Set up Wi-Fi
+    setupWiFi();
+
+    // Keep the program running
+    while (1) {
+        pi_yield();
+    }
+}
+
+int main(void) {
+    pi_bsp_init();
+
+    // Boost CPU frequency and voltage for stability
+    pi_freq_set(PI_FREQ_DOMAIN_FC, 250000000);
+    __pi_pmu_voltage_set(PI_PMU_DOMAIN_FC, 1200);
+
+    return pmsis_kickoff((void *)start_example);
 }
